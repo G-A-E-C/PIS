1919b4277cbf73b16b1b6ab385d7cb0c0b36bd6f
ApoloSantiago <santiago.apolo@unl.edu.ec>
2023-08-06 15:17:48 -0500

    Commit 1.2
    Agregación de búsqueda y ordenación en Daos

--- Proyecto-PIS/src/controlador/dao/CandidatoDao.java
+++ Proyecto-PIS/src/controlador/dao/CandidatoDao.java
@@ -1,21 +1,60 @@
 package controlador.dao;
 
 import controlador.adap.AdaptadorDao;
+import controlador.excepcion.ListaElementoNoExisteExcepcion;
+import controlador.excepcion.ListaPosicionExcepcion;
+import controlador.excepcion.ListaVaciaExcepcion;
+import controlador.lista.ListaEnlazada;
 import modelo.Candidato;
 
 public class CandidatoDao extends AdaptadorDao<Candidato> {
+
     private Candidato candidato;
 
-    public CandidatoDao(){
+    public CandidatoDao() {
         super(Candidato.class);
     }
 
     public Candidato getCandidato() {
-        if(candidato == null) candidato = new Candidato();
+        if (candidato == null) {
+            candidato = new Candidato();
+        }
         return candidato;
     }
 
     public void setCandidato(Candidato candidato) {
         this.candidato = candidato;
+    }
+
+    public Candidato buscarCandidato(String atributo, Object dato) throws ListaElementoNoExisteExcepcion, ListaPosicionExcepcion {
+
+        var candidatos = listar();
+
+        try {
+            candidatos.mergeSort(atributo, true);
+        } catch (ListaVaciaExcepcion | IllegalAccessException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+
+        try {
+            return candidatos.busquedaBinaria(atributo, dato);
+        } catch (ListaVaciaExcepcion | IllegalAccessException | ListaElementoNoExisteExcepcion | ListaPosicionExcepcion ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+
+        return null;
+    }
+
+    public ListaEnlazada<Candidato> ordenar(String atributo) {
+
+        var candidatos = listar();
+
+        try {
+            candidatos.mergeSort(atributo, true);
+        } catch (ListaVaciaExcepcion | IllegalAccessException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+
+        return candidatos;
     }
 }
--- Proyecto-PIS/src/controlador/dao/PartidoPoliticoDao.java
+++ Proyecto-PIS/src/controlador/dao/PartidoPoliticoDao.java
@@ -4,10 +4,11 @@ import controlador.adap.AdaptadorDao;
 import controlador.excepcion.ListaElementoNoExisteExcepcion;
 import controlador.excepcion.ListaPosicionExcepcion;
 import controlador.excepcion.ListaVaciaExcepcion;
+import controlador.lista.ListaEnlazada;
 import modelo.PartidoPolitico;
 
 public class PartidoPoliticoDao extends AdaptadorDao<PartidoPolitico> {
-    private PartidoPolitico partidoPolitico;
+  private PartidoPolitico partidoPolitico;
 
     public PartidoPoliticoDao(){
         super(PartidoPolitico.class);
@@ -18,16 +19,39 @@ public class PartidoPoliticoDao extends AdaptadorDao<PartidoPolitico> {
         return partidoPolitico;
     }
 
-    public PartidoPolitico buscarPartido(int id) throws ListaPosicionExcepcion, ListaVaciaExcepcion, IllegalAccessException, ListaElementoNoExisteExcepcion {
-
-        var listaPartido = listar();
-
-        listaPartido.mergeSort("id", true);
-
-        return listaPartido.busquedaBinaria("id", id);
+    public PartidoPolitico buscarPartidoPolitico(String atributo, Object dato){
+        
+        var partidos = listar();
+        
+        try {
+            partidos.mergeSort(atributo, true);
+        } catch (ListaVaciaExcepcion | IllegalAccessException ex) {
+            System.out.println("Error: "+ ex.getMessage());
+        }
+        
+        try {
+            return partidos.busquedaBinaria(atributo, dato);
+        } catch (ListaVaciaExcepcion | IllegalAccessException | ListaElementoNoExisteExcepcion | ListaPosicionExcepcion ex) {
+            System.out.println("Error: "+ ex.getMessage());
+        }
+        
+        return null;
     }
 
     public void setPartidoPolitico(PartidoPolitico partidoPolitico) {
         this.partidoPolitico = partidoPolitico;
+    }
+    
+    public ListaEnlazada<PartidoPolitico> ordenar(String atributo){
+        
+        var partidos = listar();
+        
+        try {
+            partidos.mergeSort(atributo, true);
+        } catch (ListaVaciaExcepcion | IllegalAccessException ex) {
+            System.out.println("Error: "+ ex.getMessage());
+        }
+        
+        return partidos;
     }
 }
